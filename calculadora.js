dd0555459ed756fe27669f4bb89886fd64eceba5
Eraclio Gabriel Oliveira Lopes <84460737+eracliogabriel@users.noreply.github.com>
2022-05-27 18:13:59 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -26,27 +26,4 @@
             }
         }
 
-    var dragMe = document.getElementById("calcdrag"),
-        dragOfX = 0,
-        dragOfY = 0;
-    
-function dragStart(e) {
-    dragOfX = e.pageX - dragMe.offsetLeft;
-    dragOfY = e.pageY - dragMe.offsetTop;
-    
-    addEventListener("mousemove", dragMove);
-    addEventListener("mouseup", dragEnd);
-}
-    
-function dragMove(e) {
-    /* atualiza a posição do elemento */
-    dragMe.style.left = (e.pageX - dragOfX) + 'px';
-    dragMe.style.top = (e.pageY - dragOfY) + 'px';
-}
-    
-function dragEnd() {
-    removeEventListener("mousemove", dragMove);
-    removeEventListener("mouseup", dragEnd);
-}
-dragMe.addEventListener("mousedown", dragStart);
     </script>
